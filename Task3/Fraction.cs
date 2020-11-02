90d5eafafef853dacae630660493316f2a747d75
AlekTananykin <tananykin@mail.ru>
2020-11-03 00:03:25 +0300

    Task3 Lesson2

---
+++ Task3/Fraction.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+namespace Task3
+{
+    class Fraction
+    {
+        public Fraction()
+        {
+            Numerator = 0;
+            Denomerator = 1;
+        }
+
+        public int Numerator{ get; set; }
+
+        private int _denomerator;
+        public int Denomerator 
+        {
+            get
+            {
+                return _denomerator;
+            }
+            set 
+            {
+                if (0 == value)
+                    throw new ArgumentException("Знаменатель не может быть равным \"0\"");
+                _denomerator = value;
+            } 
+        }
+
+        public Fraction Plus(Fraction number1) 
+        {
+            Fraction result = new Fraction();
+
+            result.Denomerator = Denomerator * number1.Denomerator;
+            result.Numerator = Numerator * number1.Denomerator + 
+                Denomerator * number1.Numerator;
+
+            result.Simplify();
+
+            return result;
+        }
+
+        public Fraction Subtract(Fraction number1)
+        {
+            Fraction result = new Fraction();
+
+            result.Denomerator = Denomerator * number1.Denomerator;
+            result.Numerator = Numerator * number1.Denomerator -
+                Denomerator * number1.Numerator;
+
+            result.Simplify();
+
+            return result;
+        }
+
+        public Fraction Multiply(Fraction number1)
+        {
+            Fraction result = new Fraction();
+
+            result.Numerator = Numerator * number1.Numerator;
+            result.Denomerator = Denomerator * number1.Denomerator;
+            
+            result.Simplify();
+
+            return result;
+        }
+
+        public Fraction Divide(Fraction number1)
+        {
+            Fraction result = new Fraction();
+
+            if (0 == number1.Numerator)
+                throw new ArgumentException("Ошибка деления на ноль.");
+
+            result.Denomerator = Denomerator * number1.Numerator;
+            result.Numerator = Numerator * number1.Denomerator;
+
+            result.Simplify();
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (1 == Denomerator)
+                return string.Format("{0}", Numerator);
+
+            return string.Format("{0}/{1}", Numerator, Denomerator);
+        }
+
+        public double ToDouble()
+        {
+            return (double)Numerator / (double)Denomerator;
+        }
+
+        public void Simplify()
+        {
+            int nod = Gcd(Math.Abs(Denomerator), Math.Abs(Numerator));
+            Numerator /= nod;
+            Denomerator /= nod;
+        }
+
+        private int Gcd(int a, int b)
+        {
+            while (0 != a && 0 != b)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a + b;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+/*Тананыкин
+ * 
+ * *Описать класс дробей - рациональных чисел, являющихся отношением двух 
+ * целых чисел. 
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+ * Написать программу, демонстрирующую все разработанные 
+ * элементы класса. Достаточно решить 2 задачи.
+
+** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+ArgumentException("Знаменатель не может быть равен 0");
+Добавить упрощение дробей.
+ */
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            const int maxRand = 10;
+            const int minRand = -10;
+
+            Fraction number1 = new Fraction()
+            {
+                Numerator = rnd.Next(minRand, maxRand),
+                Denomerator = rnd.Next(1, maxRand)
+            };
+
+            number1.Simplify();
+
+            int factor = rnd.Next(1, maxRand);
+            Fraction number2 = new Fraction()
+            {
+                Numerator = rnd.Next(minRand, maxRand) * factor,
+                Denomerator = rnd.Next(1, maxRand) * factor
+            };
+
+            Console.WriteLine("Первое дробное число: {0}", number1.ToString());
+            Console.WriteLine("Второе дробное число: {0}", number2.ToString());
+
+            Console.WriteLine("\nВторое дробное число в формате с плавающей " + 
+                "точкой: {0}", number2.ToDouble());
+
+            number2.Simplify();
+            Console.WriteLine("\nУпрощение второго дробного числа: {0}", number2);
+            Console.WriteLine("Второе дробное число в формате с плавающей " + 
+                "точкой: {0}", number2.ToDouble());
+
+
+            Console.WriteLine("\nОперации с дробными числами");
+
+            Fraction result = number1.Plus(number2);
+            Console.WriteLine("({0}) + ({1}) = {2}", 
+                number1.ToString(), number2.ToString(), result.ToString());
+
+            result = number1.Subtract(number2);
+            Console.WriteLine("({0}) - ({1}) = {2}",
+                number1.ToString(), number2.ToString(), result.ToString());
+
+            result = number1.Multiply(number2);
+            Console.WriteLine("({0}) * ({1}) = {2}",
+                number1.ToString(), number2.ToString(), result.ToString());
+
+            try
+            {
+                result = number1.Divide(number2);
+                Console.WriteLine("({0}) / ({1}) = {2}",
+                    number1.ToString(), number2.ToString(), result.ToString());
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine("Исключение при операции деления: {0}",
+                    aex.Message);
+            }
+
+            Console.WriteLine("\nПроверка на присваивание знаменателю значения \"0\"");
+            try
+            {
+                Fraction number3 = new Fraction();
+                number3.Denomerator = 0;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
